69fedd6b375160da882b0c2fc57ba654120808aa
Bence Szeplaki <szeplaki.bence@outlook.com>
2024-05-14 14:17:54 +0200

    Packaged the app, also added a way to connect to neo4j

---
+++ social/db.py
@@ -0,0 +1,40 @@
+import sqlite3
+from neomodel import db
+#from neomodel import config
+
+import click
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = db.set_connection(
+            url=current_app.config["DATABASE"]
+        )
+        # g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close_connection()
+
+def init_db():
+    db = get_db()
+
+    # with current_app.open_resource('schema.sql') as f:
+    #     db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
